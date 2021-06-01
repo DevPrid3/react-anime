c85422068e0db2118e0a0a2c1c809ef9dce96724
unknown <devprid3@gmail.com>
2021-06-01 23:21:56 +0200

    First commit

---
+++ src/Hero.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Anime from "react-anime";
+
+const Hero = (props) => {
+  return (
+    <Anime opacity={[0, 1]}>
+      <nav />
+    </Anime>
+  );
+};
+
+export default Hero;
